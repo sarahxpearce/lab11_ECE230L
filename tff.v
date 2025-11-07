0bb41df6917cf7fabecf214689687abdf5fe3e1d
sarahxpearce <sarahpearce@u.boisestate.edu>
2025-11-07 01:10:16 -0700

    Update tff.v

--- tff.v
+++ tff.v
@@ -1,1 +1,12 @@
-
+module tff(
+    input t,
+    input clk,
+    output q, nq
+);
+    wire d;
+    wire q_i;
+    assign d = t ? ~q_i : q_i;
+    dff u(.d(d), .clk(clk), .q(q_i));
+    assign q = q_i;
+    assign nq = ~q_i;
+endmodule
